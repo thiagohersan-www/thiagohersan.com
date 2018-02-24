9245923fc95f49472164fec5ad4de44db12f38d9
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-02-23 23:57:42 -0300

    beefs up CSP

--- js/navbar.js
+++ js/navbar.js
@@ -7,3 +7,6 @@ function hideMenu() {
   document.getElementById('navbar').classList.remove("menu-show");
   document.getElementById('navbar').classList.add("menu-hide");
 }
+
+$( ".hide-menu-icon" ).click(hideMenu);
+$( ".show-menu-icon" ).click(showMenu);
